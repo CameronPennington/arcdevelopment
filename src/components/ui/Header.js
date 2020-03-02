c87620631fbc9a0e6285463195f83ebe34f82448
CameronPennington <cameron.pennington@gmail.com>
2020-03-02 06:43:05 -0500

    added menuoptions array

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,142 +1,241 @@
-import React, {useState, useEffect} from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import {makeStyles} from '@material-ui/styles'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import Button from '@material-ui/core/Button'
-import {Link} from 'react-router-dom'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-import logo from '../../assets/logo.svg'
+import logo from "../../assets/logo.svg";
 
-const useStyles = makeStyles(theme =>({
-  toolbarMargin: {
-    ...theme.mixins.toolbar,
-    marginBottom: "3em"
-  },
-  logo: {
-    height: "8em"
-  },
-  tabContainer: {
-    marginLeft: 'auto'
-  },
-  tab: {
-    ...theme.typography.tab,
-    minWidth: 10,
-    marginLeft: "25px"
-  },
-  button: {
-    ...theme.typography.estimate,
-    borderRadius: "50px",
-    marginLeft: "50px",
-    marginRight: "25px",
-    height: "45px"
-  },
-  logoContainer: {
-    padding: 0,
-    "&:hover": {
-      backgroundColor: "transparent"
-    }
-  },
-  menu: {
-    backgroundColor: theme.palette.common.blue,
-    color: "white",
-    borderRadius: "0px"
-  },
-  menuItem: {
-    ...theme.typography.tab,
-    opacity: 0.7,
-    "&:hover": {
-      opacity: 1
-    }
-  }
-}))
+const useStyles = makeStyles(theme => ({
+	toolbarMargin: {
+		...theme.mixins.toolbar,
+		marginBottom: "3em"
+	},
+	logo: {
+		height: "8em"
+	},
+	tabContainer: {
+		marginLeft: "auto"
+	},
+	tab: {
+		...theme.typography.tab,
+		minWidth: 10,
+		marginLeft: "25px"
+	},
+	button: {
+		...theme.typography.estimate,
+		borderRadius: "50px",
+		marginLeft: "50px",
+		marginRight: "25px",
+		height: "45px"
+	},
+	logoContainer: {
+		padding: 0,
+		"&:hover": {
+			backgroundColor: "transparent"
+		}
+	},
+	menu: {
+		backgroundColor: theme.palette.common.blue,
+		color: "white",
+		borderRadius: "0px"
+	},
+	menuItem: {
+		...theme.typography.tab,
+		opacity: 0.7,
+		"&:hover": {
+			opacity: 1
+		}
+	}
+}));
 
 function ElevationScroll(props) {
-    const { children } = props;
+	const { children } = props;
 
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0
+	});
 
-export default function Header(props){
-  const classes = useStyles()
-  const [value, setValue] = useState(0)
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [open, setOpen] = useState(false)
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0
+	});
+}
 
-  const handleChange = (e, value) => {
-    setValue(value)
-  }
+export default function Header(props) {
+	const classes = useStyles();
+	const [value, setValue] = useState(0);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const [open, setOpen] = useState(false);
+	const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const handleClick = e =>{
-    setAnchorEl(e.currentTarget)
-    setOpen(true)
-  }
+	const handleChange = (e, value) => {
+		setValue(value);
+	};
 
-  const handleClose = e => {
-    setAnchorEl(null)
-    setOpen(false)
-  }
+	const handleClick = e => {
+		setAnchorEl(e.currentTarget);
+		setOpen(true);
+	};
 
-  useEffect(()=>{
-    if (window.location.pathname === "/" && value !== 0){
-      setValue(0)
-    }
-    if (window.location.pathname === "/services" && value !== 1){
-      setValue(1)
-    }
-    if (window.location.pathname === "/revolution" && value !== 2){
-      setValue(2)
-    }
-    if (window.location.pathname === "/about" && value !== 3){
-      setValue(3)
-    }
-    if (window.location.pathname === "/contact" && value !== 4){
-      setValue(4)
-    }
-    if (window.location.pathname === "/estimate" && value !== 5){
-      setValue(5)
-    }
-  }, value)
+	const handleClose = e => {
+		setAnchorEl(null);
+		setOpen(false);
+	};
 
-    return (
-      <React.Fragment>
-        <ElevationScroll>
-        <AppBar>
-            <Toolbar disableGutters>
-              <Button component={Link} to="/" className={classes.logoContainer} onClick={()=>setValue(0)} disableRipple>
-                <img src={logo} alt="company logo" className={classes.logo}></img>
-              </Button>
-            <Tabs className={classes.tabContainer} value={value} onChange={handleChange} indicatorColor="primary">
-              <Tab className={classes.tab} component={Link} to="/" label="Home"/>
-              <Tab className={classes.tab} component={Link} to="services" label="Services" aria-owns={anchorEl? "simple-menu" : undefined} aria-haspopup={anchorEl ? true : undefined} onMouseOver={event => handleClick(event)}/>
-              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
-              <Tab className={classes.tab} component={Link} to="/about" label="About Us"/>
-              <Tab className={classes.tab} component={Link} to="/contact"label="Contact Us"/>
-            </Tabs>
-            <Button variant="contained" color="secondary" className={classes.button}>
-              Free Estimate
-            </Button>
-            <Menu id="simple-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{onMouseLeave: handleClose}} classes={{paper: classes.menu}} elevation={0}>
-            <MenuItem onClick={()=>{handleClose(); setValue(1)}} component={Link} to="/services" classes={{root: classes.menuItem}}>Services</MenuItem>
-            <MenuItem onClick={()=>{handleClose(); setValue(1)}} component={Link} to="/customsoftware" classes={{root: classes.menuItem}}>Custom Sofware</MenuItem>
-            <MenuItem onClick={()=>{handleClose(); setValue(1)}} component={Link} to="/mobileapps" classes={{root: classes.menuItem}}>Mobile App Development</MenuItem>
-            <MenuItem onClick={()=>{handleClose(); setValue(1)}} component={Link} to="/websites" classes={{root: classes.menuItem}}>Website Development</MenuItem>
-            </Menu>
-            </Toolbar>
-        </AppBar>
-        </ElevationScroll>
-        <div className={classes.toolbarMargin} />
-        </React.Fragment>
-    )
+	const menuOptions = [
+		{ name: "Services", link: "/services" },
+		{ name: "Custom Software Development", link: "/customsoftware" },
+		{ name: "Mobile App Development", link: "/mobileapps" },
+		{ name: "Website Development", link: "/websites" }
+	];
+
+	useEffect(() => {
+		if (window.location.pathname === "/" && value !== 0) {
+			setValue(0);
+		}
+		if (window.location.pathname === "/services" && value !== 1) {
+			setValue(1);
+		}
+		if (window.location.pathname === "/revolution" && value !== 2) {
+			setValue(2);
+		}
+		if (window.location.pathname === "/about" && value !== 3) {
+			setValue(3);
+		}
+		if (window.location.pathname === "/contact" && value !== 4) {
+			setValue(4);
+		}
+		if (window.location.pathname === "/estimate" && value !== 5) {
+			setValue(5);
+		}
+	}, value);
+
+	return (
+		<React.Fragment>
+			<ElevationScroll>
+				<AppBar>
+					<Toolbar disableGutters>
+						<Button
+							component={Link}
+							to="/"
+							className={classes.logoContainer}
+							onClick={() => setValue(0)}
+							disableRipple
+						>
+							<img src={logo} alt="company logo" className={classes.logo}></img>
+						</Button>
+						<Tabs
+							className={classes.tabContainer}
+							value={value}
+							onChange={handleChange}
+							indicatorColor="primary"
+						>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/"
+								label="Home"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="services"
+								label="Services"
+								aria-owns={anchorEl ? "simple-menu" : undefined}
+								aria-haspopup={anchorEl ? true : undefined}
+								onMouseOver={event => handleClick(event)}
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/revolution"
+								label="The Revolution"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/about"
+								label="About Us"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/contact"
+								label="Contact Us"
+							/>
+						</Tabs>
+						<Button
+							variant="contained"
+							color="secondary"
+							className={classes.button}
+						>
+							Free Estimate
+						</Button>
+						<Menu
+							id="simple-menu"
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							MenuListProps={{ onMouseLeave: handleClose }}
+							classes={{ paper: classes.menu }}
+							elevation={0}
+						>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/services"
+								classes={{ root: classes.menuItem }}
+							>
+								Services
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/customsoftware"
+								classes={{ root: classes.menuItem }}
+							>
+								Custom Sofware
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/mobileapps"
+								classes={{ root: classes.menuItem }}
+							>
+								Mobile App Development
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/websites"
+								classes={{ root: classes.menuItem }}
+							>
+								Website Development
+							</MenuItem>
+						</Menu>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<div className={classes.toolbarMargin} />
+		</React.Fragment>
+	);
 }
